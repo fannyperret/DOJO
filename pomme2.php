d1a5f30b5c2f04e793fcaa93b8e55869ce4c32ff
Funny Rose <fannyperretlombard@gmail.com>
2017-11-21 10:38:27 +0100

    new exercice

---
+++ pomme2.php
@@ -0,0 +1,26 @@
+<?php
+
+$maxPomme = 100;
+$panier = 0;
+$price = 3.45; // prix au kilo des pommes de plus de 100gr
+$priceJuice = 1.57; // prix au kilo des pommes de moins de 100gr
+$juiceCost = $panier; // prix au kilo des pommes à jus
+$poidsPanier = 0;
+$poidsPommeJus = 0;
+$coeffTransform = 0.75;
+
+for($i = 0; $i < $maxPomme; $i++) {
+    $pomme = rand(50, 150);
+    if ($pomme >= 100) {
+        $poidsPanier += $pomme;
+        $panier++;
+    } else {
+        $poidsPommeJus += $pomme;
+    }
+}
+$poidsPommeJus = ($maxPomme - $panier) * $pomme
+
+echo "Pommes dans mon panier : " . $panier . PHP_EOL;
+echo "Pommes pour le jus : " . ($maxPomme - $panier) . PHP_EOL;
+echo "Prix du panier : " . round(($poidsPanier / 1000) * $price, 2) . PHP_EOL;
+echo "Pommes dans mon panier : " . $juiceCost . PHP_EOL;
