2df4dc1013058577124ab427fd8f04c92c9b1058
Funny Rose <fannyperretlombard@gmail.com>
2017-12-05 14:31:41 +0100

    last exercice

--- Bubble.php
+++ Bubble.php
@@ -12,13 +12,13 @@ class Bubble
     public static function generateArray($number) // génération du tableau aléatoire
     {
         $result = [];
-        for($i = 1; $i <= $number; $i++) {
-            $result[] = rand(1, 100);
+        for($i = 0; $i <= $number; $i++) {
+            $result[] = rand(1, 1000);
         }
         return $result;
     }
 
-    public static function sort(&$data) // fonction de tri à bulles
+    public static function sort1(&$data) // fonction de tri à bulles
     {
         // on parcourt le tableau du début à la fin.
         // si l'élément n > n+1 on échange n et n+1
@@ -28,7 +28,7 @@ class Bubble
 
         do {
             $swap = false;
-            for($i = 0; $i < count($data); $i++){
+            for($i = 0; $i < count($data) -1; $i++){
                 if ($data[$i] > $data[$i + 1 ]){
                     self::swap($data, $i);
                     $swap = true;
@@ -37,16 +37,39 @@ class Bubble
         } while($swap === true);
     }
 
-    private static function swap($data, $i)
+    public static function sort2(&$data)
+    {
+        $start = self::getTime();
+        $lastSwap = count($data);
+        $currentSwap = 0;
+        do {
+            $swap = false;
+            for($i = 1; $i < $lastSwap; $i++){
+                if ($data[$i] < $data[$i + 1 ]){
+                    self::swap($data, $i);
+                    $swap = true;
+                    $currentSwap = $i;
+                }
+            }
+            $lastSwap = $currentSwap;
+        } while($swap === true);
+        send = self
+    }
+
+    private static function swap(&$data, $i)
     {
         $temp = $data[$i];
         $data[$i] = $data[$i + 1];
         $data[$i + 1] = $temp;
     }
 
+    public static function getTime($microTime = null){
+        $currentTime
+    }
+
     public static function displayArray($data)
     {
-        for($i = 1; $i < count($data); $i++) {
+        for($i = 0; $i < count($data); $i++) {
             echo $data[$i];
             if($i < count($data) - 1) {
                 echo ", ";
@@ -57,6 +80,7 @@ class Bubble
 
 }
 
-$data = Bubble::generateArray(100);
-Bubble::sort ($data);
+$data = Bubble::generateArray(1000);
+Bubble::sort1 ($data);
+Bubble::sort2 ($data);
 Bubble::displayArray($data);
