06e18ef598031ad75bce749b4a5c6f0a3330c40a
Funny Rose <fannyperretlombard@gmail.com>
2017-12-05 09:56:38 +0100

    add bubble sort

--- Bubble.php
+++ Bubble.php
@@ -17,6 +17,33 @@ class Bubble
         }
         return $result;
     }
+
+    public static function sort(&$data) // fonction de tri à bulles
+    {
+        // on parcourt le tableau du début à la fin.
+        // si l'élément n > n+1 on échange n et n+1
+        // on recommence tant qu'il y a au moins 1 échange
+        // faire le tri à Bulle
+        // faire les échanges directement dans la variable $data
+
+        do {
+            $swap = false;
+            for($i = 0; $i < count($data); $i++){
+                if ($data[$i] > $data[$i + 1 ]){
+                    self::swap($data, $i);
+                    $swap = true;
+                }
+            } // on démarre à 0 car un tableau commence TOUJOURS par 0
+        } while($swap === true);
+    }
+
+    private static function swap($data, $i)
+    {
+        $temp = $data[$i];
+        $data[$i] = $data[$i + 1];
+        $data[$i + 1] = $temp;
+    }
+
     public static function displayArray($data)
     {
         for($i = 1; $i < count($data); $i++) {
@@ -27,14 +54,7 @@ class Bubble
         }
         echo PHP_EOL;
     }
-    public static function sort(&$data) // fonction de tri à bulles
-    {
-        // on parcourt le tableau du début à la fin.
-        // si l'élément n > n+1 on échange n et n+1
-        // on recommence tant qu'il y a au moins 1 échange
-        // faire le tri à Bulle
-        // faire les échanges directement dans la variable $data
-    }
+
 }
 
 $data = Bubble::generateArray(100);
